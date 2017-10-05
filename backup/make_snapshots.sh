2b447c313c50f23180ae6c3745abacd196c4173f
m8mble <m8mble@vivaldi.net>
2017-10-05 23:44:26 +0200

    backup: Provide alternative snapshot creation mode
    Either create new snapshots as before (rsync ontop of hardlinks)
    or just do plain hardlinking. The latter might be desirable for
    snapshots of previous snapshots (ie. for monthly snapshots kept
    longer than daily snapshots).
    
    Add configuration variable SNAPSHOT_CREATION_MODE to switch for a mode.
    Based on this, define a function which create the snapshot correctly.

--- backup/make_snapshots.sh
+++ backup/make_snapshots.sh
@@ -12,7 +12,51 @@ echo "Using config '${CONFIG}'"
 source "${CONFIG}"
 
 
+# ------------- helpers --------------------------------------
+
+
+is_callable() {
+   type -t "${@}" | grep -cE 'file|alias|function'
+}
+
+# Make a full, hard-link-only copy of LINK_DEST, if that
+# exists. Afterwards rsync from the system (SRC) into TGT.
+#
+# Do this in a single step (time for some rsync magic):
+# Use --link-dest flag which upon syncing SRC hard-links to LINK_DEST
+# iff the file in SRC is the same.
+create_rsync_snapshot() {
+   SRC="${1}"
+   TGT="${2}"
+   LINK_DEST="${3}"
+
+   RSYNC_ARGS=(-va --delete --delete-excluded \
+      "--exclude-from=${SNAPSHOT_EXCLUDES}" \
+      "$(readlink -m "${SRC}")/" \
+      "$(readlink -m "${TGT}")")
+
+   # ... with link-dest iff available
+   if [ -d "${LINK_DEST}" ]
+   then
+      RSYNC_ARGS=("${RSYNC_ARGS[@]}" "--link-dest=$(readlink -m "${LINK_DEST}")")
+   fi
+
+   # ... run it.
+   rsync "${RSYNC_ARGS[@]}"
+}
+
+# Plain hardlink copy SRC -> TGT
+create_hardlink_snapshot() {
+   SRC="${1}"
+   TGT="${2}"
+   # ... ignore everything else
+
+   cp -alv "${SRC}" "${TGT}"
+}
+
+
 # ------------- sanity checks --------------------------------------
+
 
 if ! [[ "${NUM_SNAPSHOTS}" =~ ^[-0-9]+$ ]] || (( NUM_SNAPSHOTS < 1))
 then
@@ -33,6 +77,31 @@ fi
 snapshot_path () {
    printf "%s/snapshot-%0${NUM_SNAPSHOT_PLACES}d" "${BACKUP_HOME}" "${1}"
 }
+
+if [ $(is_callable create_new_snapshot) -ne 0 ]
+then
+   echo "Config must not overwrite \`create_new_snapshot\`." >&2
+   exit 1
+fi
+
+case "${SNAPSHOT_CREATION_MODE}" in
+   'hardlink-rsync')
+      create_new_snapshot() {
+         create_rsync_snapshot "${@}"
+      }
+      ;;
+
+   'hardlink')
+      create_new_snapshot() {
+         create_hardlink_snapshot "${@}"
+      }
+      ;;
+
+   *)
+      echo "Unsupported SNAPSHOT_CREATION_MODE '${SNAPSHOT_CREATION_MODE}'." \
+         "Must be 'hardlink-rsync' or 'hardlink'." >&2
+      exit 1
+esac
 
 # ------------- the script itself --------------------------------------
 
@@ -63,31 +132,15 @@ do
    fi
 done
 
-# STEP 3: Create a new first snapshot for all SNAPSHOT_ORIGINS:
-# Make a full, hard-link-only copy of the latest snapshot, if that
-# exists. Afterwards rsync from the system into this latest snapshot.
-#
-# Do this in a single step (time for some rsync magic):
-# Use --link-dest=LDST flag which upon syncing SRC hard-links to LDST
-# iff the file in SRC is the same.
+# STEP 3: Create a new first snapshot for all SNAPSHOT_ORIGINS.
 LATEST="$(snapshot_path 0)"
 mkdir -vp "${LATEST}"
 for SRC in "${!SNAPSHOT_ORIGINS[@]}"
 do
-   RSYNC_ARGS=(-va --delete --delete-excluded \
-      "--exclude-from=${SNAPSHOT_EXCLUDES}" \
-      "$(readlink -m "${SRC}")/" \
-      "$(readlink -m "${LATEST}/${SNAPSHOT_ORIGINS[${SRC}]}")")
-
-   # ... with link-dest iff available
-   LINK_DEST="$(snapshot_path 1)/${SNAPSHOT_ORIGINS[${SRC}]}"
-   if [ -d "${LINK_DEST}" ]
-   then
-      RSYNC_ARGS=("${RSYNC_ARGS[@]}" "--link-dest=$(readlink -m "${LINK_DEST}")")
-   fi
-
-   # ... run it.
-   rsync "${RSYNC_ARGS[@]}"
+   create_new_snapshot \
+      "${SRC}" \
+      "${LATEST}/${SNAPSHOT_ORIGINS[${SRC}]}" \
+      "$(snapshot_path 1)/${SNAPSHOT_ORIGINS[${SRC}]}"
 done
 
 # STEP 4: Sync snapshot config
